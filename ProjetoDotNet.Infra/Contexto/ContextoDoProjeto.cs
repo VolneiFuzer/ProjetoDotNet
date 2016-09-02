694d0b53efbfc24adf785977989e51814b765e78
Volnei Fuzer <volnei.fuzer@gmail.com>
2016-09-02 20:16:32 -0300

    aula dot net dia 02/09/2016]

---
+++ ProjetoDotNet.Infra/Contexto/ContextoDoProjeto.cs
@@ -0,0 +1,19 @@
+﻿using ProjetoDotNet.Dominio.Entidades;
+using System.Data.Entity;
+
+namespace ProjetoDotNet.Infra.Contexto
+{
+	public class ContextoDoProjeto : DbContext
+	{
+
+		public DbSet <Pessoa> Pessoa{ get; set; }
+		public DbSet <Atividade> Atividades { get; set; }
+		public DbSet <Endereco> Endereco { get; set; }
+		public DbSet <Telefone> Telefone { get; set; }
+
+		public ContextoDoProjeto() : base("ProjetoDotNet")
+		{
+
+		}
+	}
+}
